a76d083e1775f04e70f2f0a794c4f072261c0594
pedro-programator <p.p.szczepaniak@gmail.com>
2019-08-04 16:46:11 +0200

    finished here...

--- src/main/java/pl/programator/cschool/bookshelf/RequestUrlMapper.java
+++ src/main/java/pl/programator/cschool/bookshelf/RequestUrlMapper.java
@@ -1,8 +1,15 @@
 package pl.programator.cschool.bookshelf;
 
+import fi.iki.elonen.NanoHTTPD;
+
 public class RequestUrlMapper {
 
     //TODO
     //tu skonczylem https://platform.cschool.pl/student-app/courses/277/modules/2845/lessons/3380/pages/4
+
+    private final static String ADD_BOOK_URL = "/book/add";
+    private final static String GET_BOOK_URL = "/book/get";
+    private final static String GET_ALL_BOOK_URL = "/book/getAll";
+
 
 }

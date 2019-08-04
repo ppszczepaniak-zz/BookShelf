edcfd8b201dbe0267c7d10c98b74c142d7ade3d4
pedro-programator <p.p.szczepaniak@gmail.com>
2019-08-04 16:10:34 +0200

    add readme

---
+++ src/main/java/pl/programator/cschool/bookshelf/readme.md
@@ -0,0 +1,3 @@
+#BookShelf app
+
+https://platform.cschool.pl/student-app/courses/277/modules/2845/lessons/3380/pages/1
--- src/main/java/pl/programator/cschool/bookshelf/storage/impl/StaticListBookStorageImpl.java
+++ src/main/java/pl/programator/cschool/bookshelf/storage/impl/StaticListBookStorageImpl.java
@@ -1,4 +1,23 @@
 package pl.programator.cschool.bookshelf.storage.impl;
 
-public class StaticListBookStorageImpl {
+import pl.programator.cschool.bookshelf.storage.BookStorage;
+import pl.programator.cschool.bookshelf.type.Book;
+
+import java.util.List;
+
+public class StaticListBookStorageImpl implements BookStorage {
+    @Override
+    public Book getBook(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+        return null;
+    }
+
+    @Override
+    public void addBook(Book book) {
+
+    }
 }

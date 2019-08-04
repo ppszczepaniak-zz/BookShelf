a9943199d989601e08fcab1f0a54b8274a832a3e
pedro-programator <p.p.szczepaniak@gmail.com>
2019-08-04 16:36:17 +0200

    finished here...

--- src/main/java/pl/programator/cschool/bookshelf/BookShelfApp.java
+++ src/main/java/pl/programator/cschool/bookshelf/BookShelfApp.java
@@ -10,14 +10,19 @@ public class BookShelfApp extends NanoHTTPD {
     private BookShelfApp(int port) throws IOException {
         super(port);
         start(5000, false);
-        System.out.println("I've started your server");
+        System.out.println("I've started your server...");
     }
 
     public static void main(String[] args) {
         try {
             new BookShelfApp(8080);
         } catch (IOException e) {
-            System.err.println("Can't start your server. Error: \n" + e);
+            System.err.println("Can't start your server! Error: \n" + e);
         }
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        return super.serve(session);
     }
 }
---
+++ src/main/java/pl/programator/cschool/bookshelf/RequestUrlMapper.java
@@ -0,0 +1,8 @@
+package pl.programator.cschool.bookshelf;
+
+public class RequestUrlMapper {
+
+    //TODO
+    //tu skonczylem https://platform.cschool.pl/student-app/courses/277/modules/2845/lessons/3380/pages/4
+
+}

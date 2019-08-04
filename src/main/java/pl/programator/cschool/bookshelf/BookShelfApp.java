bb7fef76b8930fc40fec14e18a30726aa7a4a017
pedro-programator <p.p.szczepaniak@gmail.com>
2019-08-04 15:50:36 +0200

    add server start

---
+++ src/main/java/pl/programator/cschool/bookshelf/BookShelfApp.java
@@ -0,0 +1,23 @@
+package pl.programator.cschool.bookshelf;
+
+import fi.iki.elonen.NanoHTTPD;
+
+import java.io.IOException;
+
+public class BookShelfApp extends NanoHTTPD {
+
+
+    private BookShelfApp(int port) throws IOException {
+        super(port);
+        start(5000, false);
+        System.out.println("I've started your server");
+    }
+
+    public static void main(String[] args) {
+        try {
+            new BookShelfApp(8080);
+        } catch (IOException e) {
+            System.err.println("Can't start your server. Error: \n" + e);
+        }
+    }
+}

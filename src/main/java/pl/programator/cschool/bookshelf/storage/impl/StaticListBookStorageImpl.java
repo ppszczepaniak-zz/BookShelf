df93fb3d0e5ada478fc454072744c83d28b71ef8
pedro-programator <p.p.szczepaniak@gmail.com>
2019-08-04 16:16:01 +0200

    add bookStorage

--- src/main/java/pl/programator/cschool/bookshelf/storage/impl/StaticListBookStorageImpl.java
+++ src/main/java/pl/programator/cschool/bookshelf/storage/impl/StaticListBookStorageImpl.java
@@ -3,21 +3,31 @@ package pl.programator.cschool.bookshelf.storage.impl;
 import pl.programator.cschool.bookshelf.storage.BookStorage;
 import pl.programator.cschool.bookshelf.type.Book;
 
+import java.util.ArrayList;
 import java.util.List;
 
+
 public class StaticListBookStorageImpl implements BookStorage {
+
+    private static List<Book> bookStorage = new ArrayList<>();
+
     @Override
     public Book getBook(long id) {
+        for (Book book : bookStorage) {
+            if (book.getId() == id) {
+                return book;
+            }
+        }
         return null;
     }
 
     @Override
     public List<Book> getAllBooks() {
-        return null;
+        return bookStorage;
     }
 
     @Override
     public void addBook(Book book) {
-
+        bookStorage.add(book);
     }
 }
